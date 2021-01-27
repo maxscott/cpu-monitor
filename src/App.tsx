4ca78b49c65b78a239c4a89085b6f7de475a8ce1
Max Weinbrown <max.weinbrown@gmail.com>
2021-01-27 14:00:49 -0500

    CRA + typescript, tailwindcss, craco

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="h-screen bg-gradient-to-br from-blue-50 to-blue-100">
+      <header>
+        <h3 className="text-center">Coming Soon</h3>
       </header>
     </div>
   );
